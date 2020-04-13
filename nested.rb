105025ce734d2f06c24808cd43cb1e4b2d07bb0e
Elliot Blanchard <public@elliotblanchard.com>
2020-04-13 21:02:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -115,6 +115,8 @@ def changing_alan
 
 		 programmer_hash[:alan_kay][:known_for] = "GUI"
 
+		 programmer_hash[:alan_kay][:known_for]
+
 end
 
 def adding_to_dennis
@@ -135,4 +137,6 @@ def adding_to_dennis
      }
 
 		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+
+		 programmer_hash[:dennis_ritchie][:languages]
 end
